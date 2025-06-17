47348be28e952e85031eea636bb8234719f6a5b0
CHALUVADI PRASANTH KUMAR <chaluvadikumar97@gmail.com>
2025-06-17 13:57:04 +0530

    change the line for region in moule

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc" "vpc" {
-  region= "${lookup(var.vpc_details[count.index],"region")}"
+  
   count        = "${length(var.vpc_details)}"
+  region       = "${lookup(var.vpc_details[count.index],"region")}"
   cidr_block   = "${lookup(var.vpc_details[count.index],"cidr")}"
   tags = {
         Name = "${lookup(var.vpc_details[count.index],"name")}"
