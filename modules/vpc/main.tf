f36724188dd91e2d7d73c4c13f1f356b57e0fc73
CHALUVADI PRASANTH KUMAR <chaluvadikumar97@gmail.com>
2025-06-17 14:03:18 +0530

    .

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,7 +1,6 @@
 resource "aws_vpc" "vpc" {
   
   count        = "${length(var.vpc_details)}"
-  region       = "${lookup(var.vpc_details[count.index],"region")}"
   cidr_block   = "${lookup(var.vpc_details[count.index],"cidr")}"
   tags = {
         Name = "${lookup(var.vpc_details[count.index],"name")}"
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,6 +1,5 @@
 vpc_details = [
     {
-        region="us-east-1"
         cidr="12.0.0.0/24"
     }
 ]
